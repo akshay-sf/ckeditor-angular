deb495d2c3ab820bc138cfa9022cd753b737680c
Akshay Mahajan <akshay.mahajan@sourcefuse.com>
2022-05-18 20:04:33 +0530

    feat: initial implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Ckeditor5Component } from './ckeditor5/ckeditor5.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, Ckeditor5Component],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
